0e238ac43411e20a56931b4898b4a998f1dd105f
burnout87 <gabriele.barni@gmail.com>
2023-11-22 16:37:30 +0100

    overriding jupyter command

--- python-heasoft-osa/entrypoint.sh
+++ python-heasoft-osa/entrypoint.sh
@@ -8,5 +8,16 @@ cd /tmp/jovyan;
 
 source /init.sh; 
 
+# Override the jupyter command to be forward compatible with newer
+# images that no longer launch the whole server with `jupyter notebook`.
+jupyter() { 
+    if [ "$1" = "notebook" ]; 
+    then 
+        shift  
+        $(which jupyter) server $@; 
+    else $(which jupyter) $@; 
+    fi; 
+}
+
 # run the command
 $@
